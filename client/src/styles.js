4a7fa5459cac3882cc4d0598bd602ed004203972
Archana Kumari <ak31960466@gmail.com>
2022-02-22 00:21:01 +0530

    commit message

---
+++ client/src/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+    appBar: {
+        margin: '30px 0',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+}));
